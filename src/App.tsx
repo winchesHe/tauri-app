cf8b5322c15d919893ebada8eb1e22a394b3ff15
winches <329487092@qq.com>
2024-06-12 23:31:24 +0800

    feat: add nextui navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 
+import {NextUIProvider} from "@nextui-org/react";
 import "./App.css";
 import {IndexPage} from "./pages/IndexPage";
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-    </Routes>
+    <NextUIProvider navigate={navigate}>
+      <Routes>
+        <Route element={<IndexPage />} path="/" />
+      </Routes>
+    </NextUIProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import {NextUIProvider} from "@nextui-org/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
@@ -6,10 +5,8 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <NextUIProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </NextUIProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>,
 );
